6a6924a4737afdeb73a696446b7809c8a1c505f9
MaEnqiMark <96389187+MaEnqiMark@users.noreply.github.com>
2025-11-25 15:53:19 -0500

    First Commit - Created Project Folder

---
+++ AI-Calendar/AI-Calendar/AI_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_CalendarApp.swift
+//  AI-Calendar
+//
+//  Created by 马恩奇 on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct AI_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
